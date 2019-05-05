4de358cfcadf9a586aea216cb5a46eda19b320b1
yusufselimkaratas <e226369@metu.edu.tr>
2019-05-05 10:16:25 +0300

    third tutorial

--- polls/views.py
+++ polls/views.py
@@ -2,9 +2,13 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+from .models import Sorular
+
 
 def index(request):
-    return HttpResponse("Hoşgeldin kardo.")
+    son_soru_listesi=Sorular.objects.order_by('-tarih')[:5]
+    output=', '.join([q.soru_metni for q in son_soru_listesi])
+        return HttpResponse(output)
 
 def detail(request,soru_id):
     return HttpResponse("Şu sorulara bakıyorsunuz %s" %soru_id)
